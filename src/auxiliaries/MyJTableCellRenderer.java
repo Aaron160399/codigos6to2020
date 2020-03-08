3986e65b19b593d38085523d51c921cc6ce23535
Aaron160399 <aaronlr160399@hotmail.com>
2020-03-07 21:02:44 -0600

    Imágenes en tablas
    Se agregó al proyecto un ejemplo de cómo agregar imágenes a los JTable

---
+++ src/auxiliaries/MyJTableCellRenderer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package auxiliaries;
+
+import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ *
+ * @author Aaron
+ */
+
+//Debemos heredar de la clase DefaultTableCellRenderer
+public class MyJTableCellRenderer extends DefaultTableCellRenderer{
+
+    //Debemos utilizar el siguiente método (utilicen el predictor de código)
+    @Override
+    public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int i, int i1) {
+        //Preguntamos si el elemento que se encuentra en la celda el del tipo JLabel
+        if (o instanceof JLabel) {
+            //Si es así, simplemente lo devolvemos
+            JLabel jLabel = (JLabel)o;
+            return jLabel;
+        }
+        return super.getTableCellRendererComponent(jtable, o, bln, bln1, i, i1); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
